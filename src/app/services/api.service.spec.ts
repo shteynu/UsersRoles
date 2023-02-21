817df880d1684405f70464c4acd5c8e6c9e4434b
u729427 <maxim.berenshtein@finastra.com>
2023-02-22 01:32:23 +0200

    tests fixed

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,10 +1,14 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ApiService } from './api.service';
-import {modules} from "../models/module-util";
+import {ApiService} from './api.service';
+import {modules} from '../models/module-util';
+import {of} from 'rxjs';
+import {RequestData, ResponseData} from '../models/auth-data';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpClientSpy: any;
+  let snackSpy: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,10 +16,29 @@ describe('ApiService', () => {
         ...modules
       ],
     });
-    service = TestBed.inject(ApiService);
+    httpClientSpy = {
+      get: jest.fn(),
+      post: jest.fn()
+    },
+    service = new ApiService(httpClientSpy, snackSpy);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call get with response', async () => {
+    const reqData = {} as RequestData;
+    const resData = {} as ResponseData;
+    jest.spyOn(httpClientSpy, 'get').mockReturnValue(of(resData));
+
+    await service.get(reqData).subscribe(
+      {
+        next: data => {
+          expect(data).toEqual(resData);
+        }
+      }
+    );
   });
 });
